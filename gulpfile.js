1f43bf86553ec6cc478ef782acb3ec9d0c551934
bruce <ghleed@gmail.com>
2017-10-30 22:50:32 +0800

    generate jison parser

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 const gulp = require('gulp');
+const concat = require('gulp-concat');
 const rollup = require('rollup');
 const resolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
 const babel = require('rollup-plugin-babel');
+const exec = require('child_process').exec;
 
 // rollup build config
 const config = {
@@ -21,6 +23,19 @@ const config = {
         format: 'umd'
     }
 };
+
+gulp.task('jison:build', function(cb) {
+    exec('npm run jison -- -m js src/handlebars.yy src/handlebars.l -o src/handlebars.js', function(err) {
+        if (err) return cb(err);
+        cb();
+      });
+});
+
+gulp.task('generate:parser', ['jison:build'], function(cb) {
+    return gulp.src(['src/parser-prefix.js', 'src/handlebars.js', 'src/parser-suffix.js'])
+        .pipe(concat('parser.js'))
+        .pipe(gulp.dest('lib/handlebars/compiler'));
+});
 
 gulp.task('build', function () {
     const output = config.output;
